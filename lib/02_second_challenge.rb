fe8b941ac4b39a804e2e8c094bba89670eabf6e3
brave-ping-4201 <h_yazie@yahoo.com>
2020-05-15 03:32:49 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,10 +1,10 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
+    :montague => {:patriarch => {}, :matriarch => {}, :heroine => {}, :heroine_friends => []
+      
       }, 
     :capulet => {
-      #your key/value pairs here!
+      
       }}
 
   
